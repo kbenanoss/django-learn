b38ed952a8fff78ea84604c9a3525ed4a46a4ff8
Anani <kbenanoss@gmail.com>
2024-01-25 21:10:01 +0000

    added main app

---
+++ main/admin.py
@@ -0,0 +1,8 @@
+from django.contrib import admin
+from main.models import Settings
+
+# Register your models here.
+
+admin.site.register(Settings)
+
+
---
+++ main/models.py
@@ -0,0 +1,18 @@
+from django.db import models
+
+# Create your models here.
+
+class Settings(models.Model):
+    title = models.CharField(max_length=100)
+    email = models.EmailField()
+    phone = models.CharField(max_length=100, blank=True, null=True)
+    address = models.TextField(max_length=100, blank=True, null=True)
+    fb = models.CharField(max_length=100, blank=True, null=True)
+    x = models.CharField(max_length=100, blank=True, null=True)
+
+    class Meta:
+        verbose_name_plural = 'Settings'
+
+    def __str__(self):
+        return self.title   
+
